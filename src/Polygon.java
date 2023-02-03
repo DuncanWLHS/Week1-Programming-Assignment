52c0d33e7c11b0bc6690b39c090542c430fd4dc8
Duncan Duffield <Duncan.C.Duffield@gmail.com>
2023-02-02 20:45:00 -0800

    Added Constructors and Methods. Condition: Working.
    Required:
    Perimeter Calculation.

--- src/Polygon.java
+++ src/Polygon.java
@@ -13,13 +13,74 @@ public class Polygon {
         numSides = 3;
         sideLength = 1.0;
         polygonPerimeter = 3.0;
+        shapeType = "triangle";
     }
 
     //Overloaded Polygon
-    public Polygon(double numSides, double sideLength, String shapeType) {
+    public Polygon(int numSides, double sideLength, String shapeType) {
         this.numSides = numSides;
         this.sideLength = sideLength;
+        this.shapeType = shapeType;
 
+    }
+
+    //Accessors and Mutators
+    public int getNumSides() {
+        return numSides;
+    }
+    public void setSideLength(double sideLength){
+        if (sideLength >= 0) {
+            this.sideLength = sideLength;
+        }else{
+            this.sideLength = 0.0;
+        }
+    }
+    public double getSideLength() {
+        return sideLength;
+    }
+    public void setShapeType(String shapeType) {
+
+        if (numSides < 0) {
+            this.shapeType = "Invalid Number of Sides";
+        }
+        else if (numSides == 3) {
+            this.shapeType = "triangle";
+        }
+        else if (numSides == 4) {
+            this.shapeType = "Square";
+        }
+        else if (numSides == 5) {
+            this.shapeType = "Pentagon";
+        }
+        else if (numSides == 6) {
+            this.shapeType = "Hexagon";
+        }
+        else if (numSides == 7) {
+            this.shapeType = "Heptagon";
+        }
+        else if (numSides == 8) {
+            this.shapeType = "Octagon";
+        }
+        else if (numSides == 9) {
+            this.shapeType = "Nonagon";
+        }
+        else if (numSides == 10) {
+            this.shapeType = "Decagon";
+        }else{
+            this.shapeType = "Out of Scope";
+        }
+    }
+    public String getShapeType(){
+        return shapeType;
+    }
+    public String toString() {
+        if (this.numSides < 3 || this.sideLength < 0) {
+         return "This is an invalid Polygon!";
+        } else {
+            return "Your shape is a " + shapeType + " and it has " + numSides + " sides.\n" +
+                    "It has a side length of " + sideLength + "\n" +
+                    "It has a perimeter of 34.325 units.\n";
+        }
     }
 
 }
--- src/PolygonTester.java
+++ src/PolygonTester.java
@@ -1,8 +1,19 @@
 public class PolygonTester {
     public static void main(String[] args) {
+        System.out.println("\nThis is the output with 5 sides, side length of 6.865, and a name of square");
+        Polygon poly = new Polygon(4,6.9,"square");
+        System.out.println(poly);
 
-        //default Object
-        Polygon original = new Polygon();
-        System.out.println(original);
+        System.out.println("\nThis is the output with 10 sides, side length of 1.0, and a name of decagon");
+        Polygon poly1 = new Polygon(10,1.5,"decagon");
+        System.out.println(poly1);
+
+        System.out.println("\nThis is the output using the default constructor.");
+        Polygon poly2 = new Polygon();
+        System.out.println(poly2);
+
+        System.out.println("\nThis is an example output for an invalid polygon.");
+        Polygon poly3 = new Polygon(3,-3,"Bad Polygon");
+        System.out.println(poly3);
     }
 }
