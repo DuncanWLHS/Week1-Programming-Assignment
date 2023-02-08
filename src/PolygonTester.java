08c66bcf868daa4cc50af1e492d978bd8d1bd6f4
Duncan Duffield <duffied1@wlhs.wlwv.k12.or.us>
2023-02-08 08:52:11 -0800

    Updated Files

--- src/PolygonTester.java
+++ src/PolygonTester.java
@@ -1,17 +1,7 @@
-/**
- * @Date 02/04/2023
- * @author Duncan Duffield
- * @Description This class generates polygon objects from the Polygon.java class.
- */
-
 public class PolygonTester {
     public static void main(String[] args) {
-        System.out.println("\nThis is the output with 4 sides, side length of 1, and a name of square.");
-        Polygon poly = new Polygon(4,1,"square");
-        System.out.println(poly);
-
-        System.out.println("\nThis is the output with 10 sides, side length of 1.0, and a name of decagon.");
-        Polygon poly1 = new Polygon(10,2,"decagon");
+        System.out.println("\nThis is the output with 5 sides, side length of 6.865, and a name of pentagon");
+        Polygon poly1 = new Polygon(5,6.865,"pentagon");
         System.out.println(poly1);
 
         System.out.println("\nThis is the output using the default constructor.");
@@ -19,7 +9,7 @@ public class PolygonTester {
         System.out.println(poly2);
 
         System.out.println("\nThis is an example output for an invalid polygon.");
-        Polygon poly3 = new Polygon(3,-3,"Bad Polygon");
+        Polygon poly3 = new Polygon(1,785,"line");
         System.out.println(poly3);
     }
 }
